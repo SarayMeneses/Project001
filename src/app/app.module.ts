384840695172ab107c17bf1bdc9618f946be74ed
saray <gonzalezmeneses1234@gmail.com>
2023-02-02 10:29:19 -0500

    Address

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Service1Component } from './service1/service1.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    Service1Component
+    Service1Component,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
